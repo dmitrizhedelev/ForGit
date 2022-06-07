485780aeead46d7cd98739e2b3ba0d336dd6772c
dima <dima@mail.ru>
2022-06-07 07:46:38 +0300

    clear

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,3 @@
 fun main(args: Array<String>) {
-    // first commit
-    // second commit
 
-    // vetka
-    // vetka11
-    // vetka33
-    // vetka55
-    // vetka66
-
-    // text for cherry pick
-    // kek2
 }
